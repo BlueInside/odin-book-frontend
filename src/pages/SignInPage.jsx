ba40edd714a61cef0e4c3bda21ea366faac76733
Karol <kpulawski13@gmail.com>
2024-06-25 10:57:53 +0100

    Create mock SignInPage

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,3 +1,12 @@
-export default function SignIn() {
-  return <div>Sign in now</div>;
+export default function SignInPage() {
+  const handleGitHubSignIn = () => {
+    console.log('GitHub Sign In');
+  };
+
+  return (
+    <>
+      <h1>Sign in to your account</h1>
+      <button onClick={handleGitHubSignIn}>Sign in with GitHub</button>
+    </>
+  );
 }
