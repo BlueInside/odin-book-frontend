983a996eb4eeb4ef2dd57537b770d47fdc552131
Karol <kpulawski13@gmail.com>
2024-06-25 17:09:51 +0100

    Implement basic SignInPage

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import Button from '../components/Button';
 import Header from '../components/Header';
+import { useAuth } from '../hooks/useAuth';
 
 export default function SignInPage() {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
   const handleGitHubSignIn = () => {
-    console.log('GitHub Sign In');
+    window.location.href = 'http://localhost:3000/auth/github';
   };
 
   return (
@@ -14,7 +19,17 @@ export default function SignInPage() {
         <h2>Sign in to your account</h2>
 
         <div>
-          <Button onClick={handleGitHubSignIn}>Sign in with GitHub</Button>
+          {user ? (
+            <Button
+              onClick={() => {
+                navigate('/home');
+              }}
+            >
+              Continue as {user.firstName}
+            </Button>
+          ) : (
+            <Button onClick={handleGitHubSignIn}>Sign in with GitHub</Button>
+          )}
         </div>
       </div>
     </>
