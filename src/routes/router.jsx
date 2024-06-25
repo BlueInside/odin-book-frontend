acb9eb58377401f62459b063380323f2f08e6917
Karol <kpulawski13@gmail.com>
2024-06-25 17:03:16 +0100

    Add /home path to router with dummy content

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import SignInPage from '../pages/SignInPage';
+import HomePage from '../pages/HomePage';
 
-const router = createBrowserRouter([{ path: '/', element: <SignInPage /> }]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <SignInPage />,
+  },
+  {
+    path: '/home',
+    element: <HomePage />,
+  },
+]);
 
 export default router;
