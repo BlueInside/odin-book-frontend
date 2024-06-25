5a78b5a43c6e50a57e01c9eed4c99fbbecd6b352
Karol <kpulawski13@gmail.com>
2024-06-25 10:57:36 +0100

    Replace element div with actual component

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
+import SignInPage from '../pages/SignInPage';
 
-const router = createBrowserRouter([
-  { path: '/', element: <div>Hello world!</div> },
-]);
+const router = createBrowserRouter([{ path: '/', element: <SignInPage /> }]);
 
 export default router;
