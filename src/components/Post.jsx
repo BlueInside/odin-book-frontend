b045c73d6e8c9032c35c2d55d13541654705adbe
Karol <kpulawski13@gmail.com>
2024-06-26 11:15:19 +0100

    Create Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,33 @@
-export default function Post() {
-  return <div>Post</div>;
+import PropTypes from 'prop-types';
+import { FaHeart, FaComments } from 'react-icons/fa';
+
+export default function Post({ post }) {
+  return (
+    <article>
+      <div>
+        <img
+          src={post.author.profilePicture}
+          alt={`${post.author.firstName}'s avatar'`}
+        />
+        <p>{post.author.firstName}</p>
+      </div>
+      <div>
+        <p>{post.content}</p>
+      </div>
+      <div>
+        <p>
+          <FaHeart />
+          {post.likesCount}
+        </p>
+        <p>
+          <FaComments />
+          {post.comments.length}
+        </p>
+      </div>
+    </article>
+  );
 }
+
+Post.propTypes = {
+  post: PropTypes.object,
+};
