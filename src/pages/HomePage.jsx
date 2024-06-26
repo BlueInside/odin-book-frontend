a94a3347286493a4526ff37c19975fdc1d31d606
Karol <kpulawski13@gmail.com>
2024-06-26 12:08:08 +0100

    Style Header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,24 @@
 import NavigationBar from '../components/Navbar';
 import PostList from '../components/PostList';
 import { useAuth } from '../hooks/useAuth';
+import { Link } from 'react-router-dom';
 
+import {
+  HeaderContainer,
+  Logo,
+  WelcomeMessage,
+} from '../styles/HeaderStyles.styled';
 export default function HomePage() {
   const { user } = useAuth();
 
   return (
     <>
-      <div>Welcome, {user.firstName}!</div>
+      <HeaderContainer>
+        <Link to={'https://www.theodinproject.com/'}>
+          <Logo src="/src/assets/logo.svg" alt="Odin-Book Logo" />
+        </Link>
+        <WelcomeMessage>Welcome, {user.firstName}!</WelcomeMessage>
+      </HeaderContainer>
       <PostList />
       <NavigationBar />
     </>
