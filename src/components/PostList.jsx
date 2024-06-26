5deabf4d8a738cd04313c6e84424db3ccc64e31a
Karol <kpulawski13@gmail.com>
2024-06-26 11:15:26 +0100

    Create PostList component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,3 +1,19 @@
-export default function Post() {
-  return <div>Post list</div>;
+import usePosts from '../hooks/usePosts';
+import Post from './Post';
+
+export default function PostList() {
+  const { posts, loading, error } = usePosts();
+  console.log(posts, loading, error);
+
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div>Error...</div>;
+
+  return (
+    <>
+      {posts.map((post) => (
+        <Post key={post._id} post={post} />
+      ))}
+    </>
+  );
 }
