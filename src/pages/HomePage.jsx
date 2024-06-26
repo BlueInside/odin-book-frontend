b6ea168f05d91734dc196f8c8b28e39957ac0f2f
Karol <kpulawski13@gmail.com>
2024-06-26 09:01:49 +0100

    Add NavigationBar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
+import NavigationBar from '../components/Navbar';
 import { useAuth } from '../hooks/useAuth';
 
 export default function HomePage() {
   const { user } = useAuth();
 
-  if (!user) {
-    return <div>Please log in</div>;
-  }
-
-  return <div>Welcome, {user.firstName}!</div>;
+  return (
+    <>
+      <div>Welcome, {user.firstName}!</div>
+      <NavigationBar />
+    </>
+  );
 }
