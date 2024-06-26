745071989ef480f8e6473d47efb872e62340ad5b
Karol <kpulawski13@gmail.com>
2024-06-26 12:53:18 +0100

    Create test for CreatePost component

---
+++ tests/unit/CreatePost.test.jsx
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+describe('CreatePost', () => {
+  it('Should display create post form correctly', () => {});
+});
