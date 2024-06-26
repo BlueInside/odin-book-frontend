75fb80842f6b1d1a99b42e89528370f6f00efb31
Karol <kpulawski13@gmail.com>
2024-06-26 13:12:55 +0100

    Use HomeLayout in routes

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
 import SignInPage from '../pages/SignInPage';
-import HomePage from '../pages/HomePage';
 import ProtectedRoute from '../components/ProtectedRoute';
 import CreatePost from '../pages/CreatePost';
+import HomeLayout from '../components/HomeLayout';
+import PostList from '../components/PostList';
 
 const router = createBrowserRouter([
   {
@@ -11,13 +12,22 @@ const router = createBrowserRouter([
   },
   {
     path: '/',
-    element: (
-      <ProtectedRoute>
-        <HomePage />
-      </ProtectedRoute>
-    ),
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: (
+          <ProtectedRoute>
+            <PostList />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: 'post',
+        element: <CreatePost />,
+      },
+    ],
   },
-  { path: '/post', element: <CreatePost /> },
 ]);
 
 export default router;
