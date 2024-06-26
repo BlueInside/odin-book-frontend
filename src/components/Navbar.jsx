faa870c077f85c240a06752826250c3388679d73
Karol <kpulawski13@gmail.com>
2024-06-26 09:01:27 +0100

    Create navbar with 3 links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,47 @@
-export default function Navbar() {
-  return <div>Navbar</div>;
+import { NavLink } from 'react-router-dom';
+import { FaUser, FaUsers, FaPlusSquare } from 'react-icons/fa';
+import styled from 'styled-components';
+
+const Navbar = styled.nav`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: #fff;
+  border-top: 1px solid #ccc;
+  display: flex;
+  justify-content: space-around;
+  padding: 10px 0;
+`;
+
+const Link = styled(NavLink)`
+  flex: 1;
+  color: #666;
+  text-decoration: none;
+  font-size: 20px;
+  border-right: 1px solid #ccc;
+  text-align: center;
+
+  &.active {
+    color: #4267b2;
+  }
+
+  &:hover {
+    color: #4267b2;
+  }
+`;
+
+export default function NavigationBar() {
+  return (
+    <Navbar>
+      <Link to="/profile">
+        <FaUser size={30} />
+      </Link>
+      <Link to="/users">
+        <FaUsers size={30} />
+      </Link>
+      <Link to="/post">
+        <FaPlusSquare size={30} />
+      </Link>
+    </Navbar>
+  );
 }
