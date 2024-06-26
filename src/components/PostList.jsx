a758686291f72442b44a5a4ea797c0d692a87c52
Karol <kpulawski13@gmail.com>
2024-06-26 12:52:38 +0100

    Remove console.log

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,7 +4,6 @@ import { ListWrapper } from '../styles/PostListStyles.styled';
 
 export default function PostList() {
   const { posts, loading, error } = usePosts();
-  console.log(posts, loading, error);
 
   if (loading) return <div>Loading...</div>;
 
