2a8b005e9a093ca91a10195d06f6379416fb32b9
Karol <kpulawski13@gmail.com>
2024-06-26 13:12:25 +0100

    Create homeLayout

---
+++ src/components/HomeLayout.jsx
@@ -0,0 +1,32 @@
+import { Outlet } from 'react-router-dom';
+import {
+  HeaderContainer,
+  Logo,
+  WelcomeMessage,
+} from '../styles/HeaderStyles.styled';
+import NavigationBar from '../components/Navbar';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+
+export default function HomeLayout() {
+  const { user, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <HeaderContainer>
+        <Link to={'https://www.theodinproject.com/'}>
+          <Logo src="/src/assets/logo.svg" alt="Odin-Book Logo" />
+        </Link>
+        <WelcomeMessage>Welcome, {user.firstName}!</WelcomeMessage>
+      </HeaderContainer>
+      <NavigationBar />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
