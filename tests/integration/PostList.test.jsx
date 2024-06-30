a58557ad9c09c4e2d53a0f21c2647d1ad7acc2bd
Karol <kpulawski13@gmail.com>
2024-06-30 09:49:51 +0100

    Delete post tests

--- tests/integration/PostList.test.jsx
+++ tests/integration/PostList.test.jsx
@@ -1,16 +1,24 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import PostList from '../../src/components/PostList';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import usePosts from '../../src/hooks/usePosts';
+import { useAuth } from '../../src/hooks/useAuth';
+import userEvent from '@testing-library/user-event';
 
+// Mock
 vi.mock('../../src/hooks/usePosts', () => ({ default: vi.fn() }));
+vi.mock('../../src/hooks/useAuth', () => ({
+  useAuth: vi.fn(),
+}));
+globalThis.fetch = vi.fn();
 
 const postsMock = [
   {
     _id: '1',
     content: 'test content',
     author: {
+      _id: 'id1',
       firstName: 'author1',
       profilePicture: 'someUrl',
     },
@@ -22,6 +30,7 @@ const postsMock = [
     _id: '2',
     content: 'test content2',
     author: {
+      _id: 'id1',
       firstName: 'author2',
       profilePicture: 'someUrl',
     },
@@ -42,12 +51,14 @@ const postsMock = [
   },
 ];
 
-describe('Comment list', () => {
+describe('Post list', () => {
   beforeEach(() => {
     vi.clearAllMocks();
+    fetch.mockClear();
+    useAuth.mockReturnValue({ user: { id: 'id1', firstName: 'karol' } });
   });
 
-  it('Should display comments', () => {
+  it('Should display posts', () => {
     usePosts.mockReturnValue({ posts: postsMock });
     render(
       <MemoryRouter>
@@ -61,7 +72,11 @@ describe('Comment list', () => {
   });
 
   it('Should display a loading message when loading', () => {
-    usePosts.mockReturnValue({ posts: [], loading: true, error: null });
+    usePosts.mockReturnValue({
+      posts: [],
+      loading: true,
+      error: null,
+    });
     render(
       <MemoryRouter>
         <PostList />
@@ -98,5 +113,134 @@ describe('Comment list', () => {
       postsMock[0].author.profilePicture
     );
     expect(screen.getByText(`${postsMock[0].likesCount}`)).toBeInTheDocument();
+  });
+
+  it('Should display delete buttons if post author id is same as user id', async () => {
+    usePosts.mockReturnValue({ posts: postsMock, loading: false, error: null });
+
+    render(
+      <MemoryRouter>
+        <PostList />
+      </MemoryRouter>
+    );
+
+    const deletePostButtons = screen.getAllByRole('button', {
+      name: /delete post/i,
+    });
+
+    expect(deletePostButtons).toHaveLength(2);
+  });
+
+  it('Should send DELETE fetch request on button click', async () => {
+    usePosts.mockReturnValue({ posts: postsMock, loading: false, error: null });
+    fetch.mockResolvedValueOnce({
+      ok: true,
+      json: () => Promise.resolve({ message: 'Post deleted' }),
+    });
+
+    const user = userEvent.setup();
+
+    render(
+      <MemoryRouter>
+        <PostList />
+      </MemoryRouter>
+    );
+
+    const deletePostButtons = screen.getAllByRole('button', {
+      name: /delete post/i,
+    });
+
+    // Click on first post delete button
+    await user.click(deletePostButtons[0]);
+
+    expect(fetch).toHaveBeenCalledWith(`http://localhost:3000/posts/1`, {
+      credentials: 'include',
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+  });
+
+  it('Should hide removed post', async () => {
+    usePosts.mockReturnValue({ posts: postsMock, loading: false, error: null });
+    fetch.mockResolvedValueOnce({
+      ok: true,
+      json: () => Promise.resolve({ message: 'Post deleted' }),
+    });
+
+    const user = userEvent.setup();
+
+    render(
+      <MemoryRouter>
+        <PostList />
+      </MemoryRouter>
+    );
+
+    const deletePostButtons = screen.getAllByRole('button', {
+      name: /delete post/i,
+    });
+
+    expect(screen.getAllByRole('article')).toHaveLength(3);
+    // Click on first post delete button
+    await user.click(deletePostButtons[0]);
+
+    expect(fetch).toHaveBeenCalledWith(`http://localhost:3000/posts/1`, {
+      credentials: 'include',
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+
+    expect(screen.getAllByRole('article')).toHaveLength(2);
+  });
+
+  it('Should not remove the post if the delete operation fails', async () => {
+    usePosts.mockReturnValue({ posts: postsMock, loading: false, error: null });
+    fetch.mockRejectedValueOnce(new Error('Network Error'));
+
+    const user = userEvent.setup();
+    render(
+      <MemoryRouter>
+        <PostList />
+      </MemoryRouter>
+    );
+
+    const deletePostButtons = screen.getAllByRole('button', {
+      name: /delete post/i,
+    });
+    await user.click(deletePostButtons[0]);
+
+    await waitFor(() => {
+      expect(screen.getByText('Network Error')).toBeInTheDocument();
+    });
+    expect(screen.getAllByRole('article')).toHaveLength(3); // Assert that the number of posts hasn't changed
+  });
+
+  it('Displays an error message when the delete operation fails', async () => {
+    usePosts.mockReturnValue({ posts: postsMock, loading: false, error: null });
+    fetch.mockResolvedValueOnce({
+      ok: false,
+      status: 400,
+      json: () => Promise.resolve({ message: 'Incorrect post id' }),
+    });
+
+    const user = userEvent.setup();
+    render(
+      <MemoryRouter>
+        <PostList />
+      </MemoryRouter>
+    );
+
+    const deletePostButtons = screen.getAllByRole('button', {
+      name: /delete post/i,
+    });
+
+    await user.click(deletePostButtons[0]);
+
+    await waitFor(() => {
+      expect(screen.getByText('Incorrect post id')).toBeInTheDocument();
+    });
   });
 });
