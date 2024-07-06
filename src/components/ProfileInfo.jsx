4a998ed3dc17401fb4e96f09596496bed45ac978
Karol <kpulawski13@gmail.com>
2024-07-06 15:33:52 +0100

    Displays user info

---
+++ src/components/ProfileInfo.jsx
@@ -0,0 +1,71 @@
+import PropTypes from 'prop-types';
+import { format } from 'date-fns';
+
+export default function ProfileInfo({ userDetails, currentUserId }) {
+  if (!userDetails) {
+    return <div>No user details available.</div>;
+  }
+
+  const {
+    _id,
+    firstName,
+    lastName,
+    email,
+    profilePicture,
+    coverPhoto,
+    relationship,
+    bio,
+    birthday,
+    dateJoined,
+  } = userDetails;
+
+  // Default images
+  const defaultProfilePic = '#';
+  const defaultCoverPhoto = '#';
+
+  const handleFollow = () => {
+    console.log('Follow user', _id);
+  };
+  return (
+    <div>
+      <div>
+        <img src={coverPhoto || defaultCoverPhoto} alt="Cover" />
+      </div>
+
+      <div>
+        <img
+          src={profilePicture || defaultProfilePic}
+          alt={firstName + ' ' + lastName}
+        />
+        <h1>
+          {firstName} {lastName}
+        </h1>
+        {email && <p>Email: {email}</p>}
+        {relationship && <p>Relationship Status: {relationship}</p>}
+        {bio && <p>Bio: {bio}</p>}
+        {birthday && <p>Birthday: {format(new Date(birthday), 'd MMM yyy')}</p>}
+        <p>Joined: {format(new Date(dateJoined), 'd MMM yyy')}</p>
+        {/* Show follow button only if current user is not the profile owner */}
+        {currentUserId && userDetails._id !== currentUserId && (
+          <button onClick={handleFollow}>Follow</button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+ProfileInfo.propTypes = {
+  userDetails: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string,
+    email: PropTypes.string,
+    profilePicture: PropTypes.string,
+    coverPhoto: PropTypes.string,
+    relationship: PropTypes.string,
+    bio: PropTypes.string,
+    birthday: PropTypes.string,
+    dateJoined: PropTypes.string.isRequired,
+  }).isRequired,
+  currentUserId: PropTypes.string.isRequired,
+};
