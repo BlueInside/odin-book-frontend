b397f46fbca93752b8f6062ec61190503e212b23
Karol <kpulawski13@gmail.com>
2024-07-06 15:36:15 +0100

    Update navbar test

--- tests/unit/Navbar.test.jsx
+++ tests/unit/Navbar.test.jsx
@@ -1,9 +1,21 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import NavigationBar from '../../src/components/Navbar';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { useAuth } from '../../src/hooks/useAuth';
+
+vi.mock('../../src/hooks/useAuth');
 
 describe('Navbar', () => {
+  const mockUser = {
+    user: {
+      id: 'id1',
+      firstName: 'Karol',
+    },
+  };
+  beforeEach(() => {
+    useAuth.mockReturnValue(mockUser);
+  });
   it('Should display navbar with 3 links', () => {
     render(
       <MemoryRouter>
@@ -14,7 +26,7 @@ describe('Navbar', () => {
     const links = screen.getAllByRole('link');
     expect(links).toHaveLength(3);
     // Check if all expected links are present and correctly set up
-    expect(links[0]).toHaveAttribute('href', '/profile');
+    expect(links[0]).toHaveAttribute('href', '/profile/id1');
     expect(links[1]).toHaveAttribute('href', '/users');
     expect(links[2]).toHaveAttribute('href', '/post');
   });
