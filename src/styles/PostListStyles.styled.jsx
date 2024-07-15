8e7261d66d2b905b2391a378bbfeb929b2e1a230
Karol <kpulawski13@gmail.com>
2024-07-15 08:04:28 +0100

    Add margin to last post

--- src/styles/PostListStyles.styled.jsx
+++ src/styles/PostListStyles.styled.jsx
@@ -41,4 +41,7 @@ export const ListWrapper = styled.div`
   max-width: 600px; // similar to Facebook's post width
   margin: auto;
   padding: 20px;
+  &:last-child {
+    margin-bottom: 40px;
+  }
 `;
