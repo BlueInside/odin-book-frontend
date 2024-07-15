f01e9732b32d0db05a16bb65c70297f99132167f
Karol <kpulawski13@gmail.com>
2024-07-15 08:03:49 +0100

    Adjust test, to not use fetchProfileData

--- tests/integration/ProfilePage.test.jsx
+++ tests/integration/ProfilePage.test.jsx
@@ -3,48 +3,50 @@ import ProfilePage from '../../src/pages/ProfilePage';
 import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { useAuth } from '../../src/hooks/useAuth';
-import fetchProfileData from '../../src/utilities/fetchProfileData';
 import { useParams } from 'react-router-dom';
 
-vi.mock('../../src/hooks/useAuth', () => ({
-  useAuth: () => vi.fn(),
-}));
+globalThis.fetch = vi.fn();
+
 vi.mock('react-router-dom', async (importOriginal) => {
   const actual = await importOriginal();
   return { ...actual, useParams: vi.fn() };
 });
-vi.mock('../../src/utilities/fetchProfileData');
+
+vi.mock('../../src/hooks/useUserInfo');
 vi.mock('../../src/hooks/useAuth');
 vi.mock('../../src/components/ProfileInfo', () => ({
   default: () => <div>Profile Info Component</div>,
 }));
-vi.mock('../../src/components/DisplayUserPosts', () => ({
-  default: () => <div>User Posts Component</div>,
+vi.mock('../../src/components/PostList', () => ({
+  default: () => <div>User Posts List</div>,
 }));
 
 describe('ProfilePage', () => {
   beforeEach(() => {
     useParams.mockReturnValue({ userId: '123' });
     useAuth.mockReturnValue({ user: { id: '123', firstName: 'Karol' } });
-    fetchProfileData.mockClear();
+    fetch.mockClear();
   });
 
   it('renders loading state initially', () => {
-    fetchProfileData.mockReturnValue(new Promise(() => {})); // Never resolving promise to simulate loading
+    fetch.mockReturnValueOnce(new Promise(() => {}));
+
     render(
       <MemoryRouter>
         <ProfilePage />
       </MemoryRouter>
     );
+
     expect(screen.getByText('Loading...')).toBeInTheDocument();
   });
 
   it('displays data upon successful fetch', async () => {
     const mockData = {
-      userDetails: { user: { id: '123', firstName: 'John Doe' } },
-      posts: [{ id: '1', content: 'First Post' }],
+      user: { id: '123', firstName: 'John Doe' },
     };
-    fetchProfileData.mockResolvedValue(mockData);
+
+    fetch.mockResolvedValue({ json: () => Promise.resolve(mockData) });
+
     render(
       <MemoryRouter>
         <ProfilePage />
@@ -53,11 +55,12 @@ describe('ProfilePage', () => {
 
     await waitFor(() => {
       expect(screen.getByText('Profile Info Component')).toBeInTheDocument();
-      expect(screen.getByText('User Posts Component')).toBeInTheDocument();
+      expect(screen.getByText('User Posts List')).toBeInTheDocument();
     });
   });
+
   it('handles errors during data fetching', async () => {
-    fetchProfileData.mockRejectedValue(new Error('Failed to fetch data'));
+    fetch.mockRejectedValue(new Error('Failed to fetch data'));
     render(
       <MemoryRouter>
         <ProfilePage />
