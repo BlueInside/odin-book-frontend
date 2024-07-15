79152fb519b7f45600a64fe9df980d1629d27b31
Karol <kpulawski13@gmail.com>
2024-07-15 08:41:06 +0100

    User RouterProvider check navigation on comment author click

--- tests/unit/Comments.test.jsx
+++ tests/unit/Comments.test.jsx
@@ -3,6 +3,7 @@ import { beforeEach, describe, expect, it, vi } from 'vitest';
 import Comments from '../../src/components/Comments';
 import { useAuth } from '../../src/hooks/useAuth';
 import userEvent from '@testing-library/user-event';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
 // Mocks
 vi.mock('../../src/hooks/useAuth', () => ({
@@ -11,17 +12,38 @@ vi.mock('../../src/hooks/useAuth', () => ({
 
 globalThis.fetch = vi.fn();
 
+const mockDeleteComment = vi.fn();
+const mockComments = [
+  {
+    _id: '1',
+    content: 'First comment',
+    author: { id: 'karol', _id: 'karol', firstName: 'Karol' },
+  },
+];
+
+const routes = [
+  {
+    path: '/',
+    element: (
+      <Comments
+        comments={mockComments}
+        postId="post1"
+        deleteComment={mockDeleteComment}
+      />
+    ),
+  },
+  {
+    path: '/profile/:userId',
+    element: <div>User profile page</div>,
+  },
+];
+
+const router = createMemoryRouter(routes, {
+  initialEntries: ['/'],
+  initialIndex: 1,
+});
+
 describe('Comments', () => {
-  const mockComments = [
-    {
-      _id: '1',
-      content: 'First comment',
-      author: { id: 'karol', firstName: 'Karol' },
-    },
-  ];
-
-  const mockDeleteComment = vi.fn();
-
   beforeEach(() => {
     vi.clearAllMocks();
     globalThis.fetch.mockClear();
@@ -29,13 +51,7 @@ describe('Comments', () => {
 
   it('displays comments', async () => {
     useAuth.mockReturnValue({ user: { id: 'id', firstName: 'name' } });
-    render(
-      <Comments
-        comments={mockComments}
-        postId={'post1'}
-        deleteComment={mockDeleteComment}
-      />
-    );
+    render(<RouterProvider router={router} />);
 
     // Wait for the component to update
     await waitFor(() => {
@@ -45,13 +61,7 @@ describe('Comments', () => {
 
   it('Should display delete buttons if comment belongs to user', async () => {
     useAuth.mockReturnValue({ user: { id: 'karol', firstName: 'Karol' } });
-    render(
-      <Comments
-        comments={mockComments}
-        postId={'post1'}
-        deleteComment={mockDeleteComment}
-      />
-    );
+    render(<RouterProvider router={router} />);
 
     expect(screen.getByRole('button', { name: /delete/i })).toBeInTheDocument();
   });
@@ -60,13 +70,7 @@ describe('Comments', () => {
     useAuth.mockReturnValue({
       user: { id: 'Christine', firstName: 'Christine' },
     });
-    render(
-      <Comments
-        comments={mockComments}
-        postId={'post1'}
-        deleteComment={mockDeleteComment}
-      />
-    );
+    render(<RouterProvider router={router} />);
 
     expect(
       screen.queryByRole('button', { name: /delete/i })
@@ -82,13 +86,8 @@ describe('Comments', () => {
     useAuth.mockReturnValue({
       user: { id: 'karol', firstName: 'Karol' },
     });
-    render(
-      <Comments
-        comments={mockComments}
-        postId={'post1'}
-        deleteComment={mockDeleteComment}
-      />
-    );
+    render(<RouterProvider router={router} />);
+
     const deletePostBtn = screen.getByRole('button', { name: /delete/i });
     await user.click(deletePostBtn);
 
@@ -112,19 +111,30 @@ describe('Comments', () => {
       user: { id: 'karol', firstName: 'Karol' },
     });
 
-    render(
-      <Comments
-        comments={mockComments}
-        postId={'post1'}
-        deleteComment={mockDeleteComment}
-      />
-    );
+    render(<RouterProvider router={router} />);
 
     const deleteButton = screen.getByRole('button', { name: /delete/i });
     await user.click(deleteButton);
 
     await waitFor(() => {
       expect(screen.getByText('Failed to delete comment')).toBeInTheDocument();
+    });
+  });
+
+  it('Click on comment name should navigate to profile page', async () => {
+    const user = userEvent.setup();
+    useAuth.mockReturnValue({ user: { id: 'id', firstName: 'name' } });
+
+    render(<RouterProvider router={router} />);
+
+    const authorLink = screen.getByRole('link', {
+      name: mockComments[0].author.firstName,
+    });
+
+    await user.click(authorLink);
+
+    await waitFor(() => {
+      expect(screen.getByText('User profile page')).toBeInTheDocument();
     });
   });
 });
