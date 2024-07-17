332eb7aba656fb75766968cac6c79eee1d0797c8
Karol <kpulawski13@gmail.com>
2024-07-17 10:57:13 +0100

    create EditForm, add closeForm and onSave props add error and isSubmitting state

--- src/components/EditProfileForm.jsx
+++ src/components/EditProfileForm.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export default function EditProfileForm({ userDetails, onSave }) {
+export default function EditProfileForm({
+  userDetails,
+  onSave,
+  closeEditForm,
+}) {
   const [formData, setFormData] = useState({
     firstName: userDetails.firstName || '',
     lastName: userDetails.lastName || '',
@@ -10,12 +14,20 @@ export default function EditProfileForm({ userDetails, onSave }) {
     relationship: userDetails.relationship || '',
     birthday: userDetails.birthday ? userDetails.birthday.substring(0, 10) : '', // Formatting date for input type="date"
   });
+
   const [profilePicture, setProfilePicture] = useState(null);
   const [coverPhoto, setCoverPhoto] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleChange = (e) => {
+    setError(null);
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
+
+    if (name === 'firstName' && value.trim().length < 2) {
+      setError('First name must be between 2-15 characters long.');
+    }
   };
 
   const handleImageChange = (e) => {
@@ -26,11 +38,15 @@ export default function EditProfileForm({ userDetails, onSave }) {
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    if (error) return;
+    setIsSubmitting(true);
 
     const data = new FormData();
-    Object.keys(formData).forEach((key) => data.append(key, formData[key]));
+    Object.keys(formData).forEach((key) => {
+      data.append(key, formData[key]);
+    });
     if (profilePicture) {
       data.append('profilePicture', profilePicture);
     }
@@ -38,85 +54,128 @@ export default function EditProfileForm({ userDetails, onSave }) {
       data.append('coverPhoto', coverPhoto);
     }
 
-    onSave(data);
+    try {
+      const response = await fetch(
+        `http://localhost:3000/users/${userDetails._id}`,
+        {
+          method: 'PUT',
+          credentials: 'include',
+          body: data,
+        }
+      );
+
+      if (!response.ok) {
+        console.log('Update response not ok: ', response);
+        if (response.status >= 400) {
+          const responseError = await response.json();
+          const errorMessage =
+            responseError.error ||
+            responseError?.errors[0]?.msg ||
+            'Undefined error occurred. Please try again later.';
+          throw new Error(errorMessage);
+        }
+      }
+
+      const userData = await response.json();
+      const updatedUser = userData.user;
+
+      if (typeof updatedUser.isFollowedByCurrentUser == 'undefined') {
+        updatedUser.isFollowedByCurrentUser = false;
+      }
+
+      console.log('Successfully updated user: ', updatedUser);
+      onSave(updatedUser);
+    } catch (error) {
+      console.log('Update user Error', error);
+      setError(error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        First Name:
-        <input
-          type="text"
-          name="firstName"
-          value={formData.firstName}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label>
-        Last Name:
-        <input
-          type="text"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Bio:
-        <textarea name="bio" value={formData.bio} onChange={handleChange} />
-      </label>
-      <label>
-        Relationship Status:
-        <select
-          name="relationship"
-          value={formData.relationship}
-          onChange={handleChange}
-        >
-          <option value="">Choose...</option>
-          <option value="Single">Single</option>
-          <option value="Complicated">Complicated</option>
-          <option value="In relationship">In relationship</option>
-        </select>
-      </label>
-      <label>
-        Birthday:
-        <input
-          type="date"
-          name="birthday"
-          value={formData.birthday}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Profile Picture:
-        <input
-          type="file"
-          name="profilePicture"
-          onChange={handleImageChange}
-          accept="image/*"
-        />
-      </label>
-      <label>
-        Cover Photo:
-        <input
-          type="file"
-          name="coverPhoto"
-          onChange={handleImageChange}
-          accept="image/*"
-        />
-      </label>
-      <button type="submit">Save Changes</button>
-    </form>
+    <div>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Bio:
+          <textarea name="bio" value={formData.bio} onChange={handleChange} />
+        </label>
+        <label>
+          Relationship Status:
+          <select
+            name="relationship"
+            value={formData.relationship}
+            onChange={handleChange}
+          >
+            <option value="">Choose...</option>
+            <option value="Single">Single</option>
+            <option value="Complicated">Complicated</option>
+            <option value="In relationship">In relationship</option>
+          </select>
+        </label>
+        <label>
+          Birthday:
+          <input
+            type="date"
+            name="birthday"
+            value={formData.birthday}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Profile Picture:
+          <input
+            type="file"
+            name="profilePicture"
+            onChange={handleImageChange}
+            accept="image/*"
+          />
+        </label>
+        <label>
+          Cover Photo:
+          <input
+            type="file"
+            name="coverPhoto"
+            onChange={handleImageChange}
+            accept="image/*"
+          />
+        </label>
+        <button type="button" onClick={closeEditForm}>
+          Cancel
+        </button>
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? 'Submitting...' : 'Save Changes'}
+        </button>
+      </form>
+    </div>
   );
 }
 
@@ -134,6 +193,6 @@ EditProfileForm.propTypes = {
     birthday: PropTypes.string,
     dateJoined: PropTypes.string.isRequired,
   }).isRequired,
-
   onSave: PropTypes.func.isRequired,
+  closeEditForm: PropTypes.func.isRequired,
 };
