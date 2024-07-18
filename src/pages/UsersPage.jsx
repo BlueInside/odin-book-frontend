05e0ed93e9ef4fe90613d5cbc8929dbb21dfaef1
Karol <kpulawski13@gmail.com>
2024-07-18 07:51:52 +0100

    basic implementation of users page

---
+++ src/pages/UsersPage.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import UsersList from '../components/UsersList';
+import PaginationControls from '../components/PaginationControls';
+
+export default function UsersPage() {
+  const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/users/?q=${encodeURIComponent(
+            query
+          )}&page=${currentPage}`
+        );
+
+        const data = await response.json();
+        setUsers(data.users);
+        setTotalPages(data.totalPages);
+        setHasNextPage(hasNextPage);
+      } catch (error) {
+        console.error('Failed to fetch users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, [currentPage, hasNextPage, query]);
+
+  return (
+    <div>
+      <label htmlFor="searchBar" aria-label="search bar">
+        Search
+      </label>
+      <input
+        type="text"
+        id="searchBar"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+        placeholder="Search users"
+      />
+
+      <UsersList />
+      <PaginationControls />
+    </div>
+  );
+}
