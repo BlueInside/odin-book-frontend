2d87cfb79b3b10d78536c01bb5327e53db375aae
Karol <kpulawski13@gmail.com>
2024-07-18 18:44:45 +0100

    add search

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,26 +1,30 @@
 import { useEffect, useState } from 'react';
 import UsersList from '../components/UsersList';
 import PaginationControls from '../components/PaginationControls';
+import { MdSearch } from 'react-icons/md';
 import {
   SearchBarContainer,
   Label,
   SearchInput,
   StyledContainer,
+  SearchButton,
 } from '../styles/UsersPageStyles.styled';
+
 export default function UsersPage() {
   const [users, setUsers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [hasNextPage, setHasNextPage] = useState(false);
+  const [searchInput, setSearchInput] = useState('');
   const [query, setQuery] = useState('');
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchUsers = async (page = currentPage) => {
       try {
         const response = await fetch(
           `http://localhost:3000/users/?q=${encodeURIComponent(
             query
-          )}&page=${currentPage}`,
+          )}&page=${page}`,
           { credentials: 'include' }
         );
         const data = await response.json();
@@ -38,18 +42,34 @@ export default function UsersPage() {
   return (
     <StyledContainer>
       <SearchBarContainer>
-        <Label htmlFor="searchBar" aria-label="search bar">
-          Search
-        </Label>
-        <SearchInput
-          type="text"
-          id="searchBar"
-          value={query}
-          onChange={(e) => {
-            setQuery(e.target.value);
+        <form
+          style={{ display: 'flex' }}
+          action=""
+          onSubmit={(e) => {
+            e.preventDefault();
+            setQuery(searchInput);
+            setCurrentPage(1);
           }}
-          placeholder="Search users"
-        />
+        >
+          <Label htmlFor="searchBar" aria-label="search bar">
+            Search
+          </Label>
+          <SearchInput
+            type="text"
+            id="searchBar"
+            value={searchInput}
+            onChange={(e) => {
+              {
+                setSearchInput(e.target.value);
+              }
+            }}
+            placeholder="Search users"
+          />
+          <SearchButton type="submit">
+            <MdSearch size="20px" />
+            Search
+          </SearchButton>
+        </form>
       </SearchBarContainer>
 
       <UsersList users={users} />
