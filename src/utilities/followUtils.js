f3f43f52e167493588becc15e00236a0d28613fe
Karol <kpulawski13@gmail.com>
2024-07-18 16:31:44 +0100

    implement follow unfollow util functions

---
+++ src/utilities/followUtils.js
@@ -0,0 +1,38 @@
+export const followUser = async (userId) => {
+  try {
+    const response = await fetch(`http://localhost:3000/follow`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({ followedId: userId }),
+    });
+
+    if (!response.ok) throw new Error('Failed to follow user.');
+
+    return response.json();
+  } catch (error) {
+    console.error('Error following user:', error);
+    return null;
+  }
+};
+
+export const unfollowUser = async (userId) => {
+  try {
+    const response = await fetch(`http://localhost:3000/unfollow`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({ followedId: userId }),
+    });
+    if (!response.ok) throw new Error('Failed to un-follow user.');
+
+    return response.json();
+  } catch (error) {
+    console.error('Error un-following user:', error);
+    return null;
+  }
+};
