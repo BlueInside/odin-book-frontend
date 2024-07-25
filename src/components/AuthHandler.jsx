4955b8cd87c25ccbc7c752f7180333929e7dd13e
Karol <kpulawski13@gmail.com>
2024-07-25 14:56:33 +0100

    get token form query save it to localStorage and navigate

--- src/components/AuthHandler.jsx
+++ src/components/AuthHandler.jsx
@@ -5,10 +5,9 @@ import LoadingSpinner from './Spinner';
 export default function AuthHandler() {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+
   useEffect(() => {
     const token = searchParams.get('token');
-
-    console.log(token);
     if (token) {
       localStorage.setItem('jwtToken', token);
       navigate('/');
@@ -18,7 +17,7 @@ export default function AuthHandler() {
         state: { error: `Authentication failed. No token provided.` },
       });
     }
-  }, [searchParams, navigate]);
+  });
 
   return <LoadingSpinner />;
 }
