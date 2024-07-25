5c73fe2aae79175f916838b36027ddf107e814dc
Karol <kpulawski13@gmail.com>
2024-07-25 14:57:08 +0100

    add state to error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from 'react-router-dom';
 import { ErrorMessage } from '../styles/Comments.styled';
 import {
   ErrorCode,
@@ -7,16 +8,25 @@ import {
 } from '../styles/ErrorPageStyles.styled';
 import PropTypes from 'prop-types';
 export default function ErrorPage() {
-  const errorCode = 'Error';
-  const message = 'Something went wrong.';
-  const suggestion = 'Please check the URL or go back to the home page.';
+  let location = useLocation();
+  const {
+    errorCode = 'Error',
+    message = 'Something went wrong.',
+    suggestion = 'Please check the URL or go back to the home page.',
+  } = location.state || {};
+
+  const notAuthenticatedUser = errorCode === 401 || errorCode === 403;
 
   return (
     <ErrorContainer>
       <ErrorCode>{errorCode}</ErrorCode>
       <ErrorMessage>{message}</ErrorMessage>
       <Suggestion>{suggestion}</Suggestion>
-      <HomeLink to="/">Go Home</HomeLink>
+      {notAuthenticatedUser ? (
+        <HomeLink to="/sign">Sign in</HomeLink>
+      ) : (
+        <HomeLink to="/">Go Home</HomeLink>
+      )}
     </ErrorContainer>
   );
 }
